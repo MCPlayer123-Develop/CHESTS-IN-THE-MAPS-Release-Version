5c689a91ba5564c03c0948edd056e37c01b3a10e
MCPlayer123-Develop <304681737@qq.com>
2026-04-29 22:26:06 +0800

    Delete cpp/Snapshot/Alpha 0.8.4-Dev-snapshot260428 directory

--- cpp/Snapshot/Alpha 0.8.4-Dev-snapshot260428/Alpha 0.8.4-Dev-snapshot260428-Project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "./Lib/game.h"
-
-int main() {
-	SetConsoleOutputCP(65001);//设为UTF-8(国际通用)
-	srand((unsigned int)time(nullptr));
-	game_overworld();
-	write_game_data();
-	return 0;
-}
--- cpp/Snapshot/Alpha 0.8.4-Dev-snapshot260428/README.txt
+++
@@ -1,2 +0,0 @@
-请运行main.cpp，便可正常游玩
-Please run "main.cpp", then you can enjoy the game.
