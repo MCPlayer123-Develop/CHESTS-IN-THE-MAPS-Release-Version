f79c8bfaf6c7dc3ec74a32bcc9162cf5a7bf132e
MCPlayer123-Develop <304681737@qq.com>
2026-08-01 22:22:06 +0800

    Updated Alpha 0.8.4-rc1

---
+++ cpp/Snapshot/Alpha 0.8.4-rc1/CITM_a0.8.4_rc1/Lib/ui.h
@@ -0,0 +1,20 @@
+#ifndef UI_H
+#define UI_H
+#include "game.h"
+
+void citm_development_team_animation();
+void options();
+int title_screen();
+void chest();
+void chest_nether();
+void shop();
+void useItem();
+void useItem_nether();
+void view_inventory();
+void eatFood();
+void view_advancements();
+void command();
+int game_menu();
+void statistic();
+
+#endif
